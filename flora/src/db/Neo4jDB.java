28dc44aaa832dd0a6db09a08094337a929792675
GuoTong <471729125@qq.com>
2019-04-27 09:08:38 +0800

    完善数据库连接

--- flora/src/db/Neo4jDB.java
+++ flora/src/db/Neo4jDB.java
@@ -1,9 +1,7 @@
 package db;
 
 
-import org.neo4j.driver.v1.AuthTokens;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.*;
 
 
 public class Neo4jDB {
@@ -11,13 +9,36 @@ public class Neo4jDB {
     private static Driver driver;
 
     public static void connectGraphDB(){
-        driver = GraphDatabase.driver("b", AuthTokens.basic("",""));
+        String uri = "bolt://localhost:7687";
+        driver = GraphDatabase.driver(uri, AuthTokens.basic("neo4j", "1234"));
+
     }
 
     public static void close(){
         driver.close();
     }
 
+    public Driver getDriver(){
+        return driver;
+    }
+
+    public static void main(String[] args) {
+
+        Neo4jDB.connectGraphDB();
+        Neo4jDB db=new Neo4jDB();
+        try (Session session = db.getDriver().session()) {
+            StatementResult result = session.run("match(n)-[r:isSubClass]->(m) where n.name<>m.name return n.name,m.name");
+            while (result.hasNext()) {
+                Record record = result.next();
+                String name = record.get("n.name").asString();
+                String refer = record.get("m.name").asString();
+
+                System.out.println(name + "的" + "父类" + "是" + refer);
+            }
+
+        Neo4jDB.close();
+        }
+    }
 
 
 }
