55e221ddf3aebd5dd86f974bcb437f04d4f1c2d0
04756 <2351735698@qq.com>
2019-05-08 21:41:31 +0800

    python调用

--- flora/src/bean/InvokePythonProject.java
+++ flora/src/bean/InvokePythonProject.java
@@ -29,13 +29,13 @@ public class InvokePythonProject extends Thread implements Callable<String> {
 //        先启动cmd，再运行命令，查看报错。
 //        该方法能够详细的查看到报错
 //        直接模拟cmd执行命令只能从waitfor返回的数字猜测原因不能查看具体错误
-        String[] com = { "cmd", };
+        String[] args = new String[] { "python", command, planet};
         Process p = null;//创建一个线程池对象
         ExecutorService pool = Executors.newCachedThreadPool(); //创建一个有返回值的任务
         String returnValue = "";//创建一个字符串存放返回结果
 
         try {
-            p = Runtime.getRuntime().exec(com);
+            p = Runtime.getRuntime().exec(args);
             out = new SyncPipe(p.getInputStream(), System.out);
             t1 = new SyncPipe(p.getErrorStream(), System.err);
             t2 = out;
